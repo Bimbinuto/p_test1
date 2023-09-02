ec62347b33898aae1483b3d91a57321262a5a470
Bimbinuto <bimbinuto@gmail.com>
2023-09-01 21:11:18 -0600

    Agregar archivos de proyecto.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjectTestBibliotk
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //TODO incluir mas detalles al Boton del diseñador XAML
+        private void Hello(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hola!");
+        }
+    }
+}
